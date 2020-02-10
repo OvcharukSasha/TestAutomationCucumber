97f82ad7b30f5e67b7495cabf5f259565ea69c29
Олександра Овчарук <oleksandraovcaruk@Oleksandras-MacBook-Pro.local>
2020-02-11 00:07:08 +0200

    configuration for cucumber report

--- src/test/java/hooks/TestRunner.java
+++ src/test/java/hooks/TestRunner.java
@@ -8,6 +8,8 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(tags = "@ui, @api",
         features = "src/test/resource"
         , glue = {"stepDefinitions"}
+        , strict = true, format = {
+        "pretty", "html:target/cucumber", "json:target/cucumber/report.json", "junit:target/cucumber/junit.xml"}
 )
 
 public class TestRunner {
